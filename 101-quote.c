b80c792493dcc9c778f634429be4cff96f6f3c63
Your Name <you@example.com>
2020-09-17 13:30:25 +0200

    test;

---
+++ 101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Where the program starts
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, q, strlen(q));
+	return (1);
+}
