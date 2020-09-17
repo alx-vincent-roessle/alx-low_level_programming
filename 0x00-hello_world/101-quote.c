37171f7f85a650e371d0e05f5df02aee744b675f
Your Name <you@example.com>
2020-09-17 13:54:34 +0200

    test;

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char q[] = "and that piece of art is useful\" -\
+	char q[] = "and that piece of art is useful\" - \
 Dora Korpar, 2015-10-19\n";
-	
+
 	write(2, q, 59);
 	return (1);
 }
